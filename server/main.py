b551685489ea6f9ddea7d0cd51c5bc947cc8df2f
Maaayank <3mayankpatel0@gmail.com>
2019-09-21 01:38:43 +0530

    updating project

---
+++ server/main.py
@@ -0,0 +1,26 @@
+from flask import Flask,render_template,Blueprint,request
+from . import db
+
+main = Blueprint('main',__name__)
+
+@main.route("/")
+def homePage() :
+    return render_template('index2.html')
+
+@main.route("/searchbus")
+def searchBusPage() :
+    return 'searchbus'
+
+@main.route("/searchbus",methods=['POST'])
+def searchBus() :
+    source = request.form['source']
+    dest = request.form['dest']
+    jdate = request.form['jdate']
+    bus_class = request.form['bus_class']
+
+    results = db.session.execute("select * from busses where busses.bus_no in(select bus_no from bus_stops where bus_stops.stop_id = (select stop_id from stops where stops.name = :source) intersect select bus_no from bus_stops where bus_stops.stop_id = (select stop_id from stops where stops.name = :dest))",{'source':source ,'dest':dest})
+    
+    return render_template('.html',results = results)
+
+
+
